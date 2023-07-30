5293b67eec69dd1490e21997f93be7d1b389be01
Mvmo <mvmogo@gmail.com>
2023-07-30 15:24:23 +0200

    Basic vga buffer implementation

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,19 @@
 #![no_std]
 #![no_main]
 
+mod vga_buffer;
+
 use core::panic::PanicInfo;
 
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
     loop {}
 }
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
+    println!("Hello, World!");
+    panic!("NOOOOO!!!");
     loop {}
 }
