b7adb533d373551b245edfccbae21c20d42511fd
Mvmo <mvmogo@gmail.com>
2023-07-30 16:22:58 +0200

    Implement unit tests and integration tests framework and some tests

--- tests/basic_boot.rs
+++ tests/basic_boot.rs
@@ -4,35 +4,23 @@
 #![test_runner(mvos::test_runner)]
 #![reexport_test_harness_main = "test_main"]
 
-use core::panic::PanicInfo;
 use mvos::println;
+use core::panic::PanicInfo;
 
-#[no_mangle]
+#[no_mangle] // don't mangle the name of this function
 pub extern "C" fn _start() -> ! {
-    println!("Hello, World {}", "!");
-
-    #[cfg(test)]
     test_main();
 
     loop {}
 }
 
-#[cfg(not(test))]
-#[panic_handler]
-fn panic(info: &PanicInfo) -> ! {
-    println!("{}", info);
-    loop {}
-}
-
-#[cfg(test)]
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
     mvos::test_panic_handler(info)
 }
 
-
 #[test_case]
-fn trivial_assertion() {
-    assert_eq!(1, 1);
+fn test_println() {
+    println!("test_println output");
 }
 
